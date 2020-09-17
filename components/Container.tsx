b025947ae8ca42e82dddaa018079cbc05b04ffc2
Alex <me@a9e.sh>
2020-09-17 06:37:58 -0700

    Minor cosmetic UI changes

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 
 const Container: FunctionComponent = ({ children }: Props) => {
-  return <div className="container mx-auto px-5">{children}</div>
+  return <div className="container mx-auto px-5 mt-6 md:mt-12">{children}</div>
 }
 
 export default Container
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,15 +4,15 @@ type Props = {
 
 function Logo({ small }: Props) {
   const textSize = small ? 'text-3xl md:text-5xl' : 'text-6xl md:text-8xl'
-  const padding = small ? 'px-2' : 'px-4'
+  const padding = small ? 'px-2 mb-1' : 'px-4 mb-2'
 
   return (
     <div
       style={{ width: 'max-content' }}
-      className={`${textSize} text-right font-bold tracking-tighter leading-tight`}
+      className={`${textSize} text-right font-bold tracking-tighter leading-tight select-none cursor-default`}
     >
       <div className={`${padding} whitespace-no-wrap`}>langara</div>
-      <div className={`rounded-b-lg ${padding} bg-black text-white`}>
+      <div className={`rounded-lg ${padding} bg-black text-white`}>
         <span className="text-brand-orange">.</span>
         cs
       </div>
